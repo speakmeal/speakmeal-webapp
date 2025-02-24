08fcd28a4ef4c099be467f57744b64225ddef988
pedro12345679-wq <getspeakmeal@gmail.com>
2025-02-24 07:05:36 +0000

    Feat: Remove uneeded sections and host asset files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 "use client"
 
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 export default function Home() {
-  // '/' router should not be accessible, since landing page is deployed on framer, 
-  // hence redirect the user to the sign up page
-  const router = useRouter();
-  useEffect(() => {
-    router.push("/SignIn");
-  }, [])
+  <div></div>
 }
