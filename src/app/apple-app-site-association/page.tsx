d258c4155a10dcabb22023c806149169fea5dd89
pedro12345679-wq <getspeakmeal@gmail.com>
2025-02-24 12:55:02 +0000

    Change to html

---
+++ src/app/apple-app-site-association/page.tsx
@@ -0,0 +1,31 @@
+export default function Page() {
+  return (
+    <p>
+{`{
+  "applinks": {
+    "apps": [],
+    "details": [
+      {
+        "appIDs": [
+          "77GN289H5C.com.speakmeal"
+        ],
+        "paths": [
+          "*"
+        ],
+        "components": [
+          {
+            "/": "/*"
+          }
+        ]
+      }
+    ]
+  },
+  "webcredentials": {
+    "apps": [
+      "77GN289H5C.com.speakmeal"
+    ]
+  }
+}`}
+    </p>
+  );
+}
